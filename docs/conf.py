f3ba1646e50ea2a182369a68ebefc0690bb0d873
lbusoni <lbusoni@gmail.com>
2020-04-05 12:12:07 +0200

    master_doc set to index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -14,13 +14,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'tipico'
 copyright = '2020, Lorenzo Busoni'
 author = 'Lorenzo Busoni'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -38,6 +36,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
